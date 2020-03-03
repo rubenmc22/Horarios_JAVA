0034e5d8900a1b57fff4f52b9254029d0da0f49b
Luis Bastidas <mbastidasluis@gmail.com>
2020-03-03 12:29:07 -0400

    Initial commit

---
+++ src/main/java/com/cpu/sistema_horario_java/SistemaHorario.java
@@ -0,0 +1,13 @@
+package com.cpu.sistema_horario_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpu/sistema_horario_java/SistemaHorarioTests.java
@@ -0,0 +1,13 @@
+package com.cpu.sistema_horario_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
