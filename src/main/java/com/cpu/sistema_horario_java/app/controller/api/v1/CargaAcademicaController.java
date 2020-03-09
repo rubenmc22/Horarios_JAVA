f00248dc5773e6f76b0d4dcf7b8fbe3b6a9f8b02
rubenmc24 <rubemm18@gmail.com>
2020-03-09 09:35:55 -0400

    Arreglo politica de Cors

--- src/main/java/com/cpu/sistema_horario_java/app/controller/api/v1/CargaAcademicaController.java
+++ src/main/java/com/cpu/sistema_horario_java/app/controller/api/v1/CargaAcademicaController.java
@@ -10,18 +10,12 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/v1/cargas_academicas")
 public class CargaAcademicaController {
 
